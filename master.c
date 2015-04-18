7c669aae7ba1e5148d488d7270165b18947ace2a
Sarah Khan <sarahjmi07@gmail.com>
2015-04-19 04:39:48 +0530

    New changes made

--- master.c
+++ master.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <curses.h>
+
 #include <sys/socket.h>
 #include <stdio.h>
 #include <netinet/in.h>
@@ -20,6 +21,7 @@ struct snake{
     int x,y;				//pos of snake
     int flag;				//
     int tempflag2;			//
+    int minitem;
 };			
 
 typedef struct snake snake;		//
@@ -29,7 +31,7 @@ int main()
     int master_len, slave_len;		//for socket
     snake *master,*slave;		//made master and slave snake pointer
     int ch;				//num replace
-    int i;			
+    int i,foodcount,flaggy;			
     int tempflag1;
     int my_score=0,other_score=0;
 	int level=0;	
@@ -72,6 +74,10 @@ int main()
     slave_len = sizeof(slave_address);
     slave_sockfd = accept(master_sockfd, 
             (struct sockaddr *)&slave_address, &slave_len);
+	if(slave_sockfd==-1)
+	{
+		printf("bagvbkujfbvgkie");
+	}
 
     initscr();
 
@@ -91,8 +97,8 @@ int main()
 	switch(level)
 	{
 		case 0: foodcount=5;
-			masterminitem=2;
-			slaveminitem=2;
+			master->minitem=2;
+			slave->minitem=2;
 			flaggy=1;
 			while(flaggy)//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@222
 			{
@@ -132,7 +138,7 @@ int main()
 				    if(master->snake_array[i][0]==master->food[1]&&master->snake_array[i][1]==master->food[0])
 				    {
 				        tempflag1=1;
-					--masterminitem;
+					--(master->minitem);
 				        break;
 				    }
 				}
@@ -141,7 +147,7 @@ int main()
 				    if(slave->snake_array[i][0]==master->food[1]&&slave->snake_array[i][1]==master->food[0])
 				    {
 				        tempflag1=1;
-					--slaveminitem;
+					--(slave->minitem);
 				        break;
 				    }
 				}
@@ -177,7 +183,7 @@ int main()
 			    (master->snakelength)++;  //snakelength badh rhi h...already implemented feature
 			    (master->score)+=10;
 			    master->tempflag2=1;
-			    --masterminitem;
+			    --(master->minitem);
 			}
 
 
@@ -210,7 +216,7 @@ int main()
 			}
 
 
-			if(masterminitem<=0 && slaveminitem<=0)
+			if(master->minitem<=0 && slave->minitem<=0)
 			{
 				if(foodcount==0)
 				{
@@ -237,7 +243,8 @@ int main()
 			//isime maze bhi banega
 			// set by ncurses display timelimit 5min;
 		case 1: foodcount=6;
-			minitem=2;
+			master->minitem=2;
+			slave->minitem=2;
 			//reading slave snake attribites
 			//reading slave snake attribites
 			read(slave_sockfd, slave, sizeof(snake));
@@ -253,11 +260,133 @@ int main()
 			hline(ACS_HLINE,COLS);
 
 			init_keyboard();
+			master->tempflag2=1;
+			slave->tempflag2=1;
+			while((master->tempflag2==1)||(slave->tempflag2==1)||foodcount!=0)                  //food generation
+			{
+			    srand(time(0));
+			    master->food[0]=(rand()%COLS-1);
+			    master->food[1]=(rand()%((5*LINES/6)-1));
+
+			    if(master->food[1]<=0||master->food[0]<=0)
+			    {
+				master->tempflag2=1;
+				slave->tempflag2=1;
+			    }
+			    else
+			    {
+
+				--foodcount;				
+				for(i=0;i<master->snakelength;i++)
+				{
+				    if(master->snake_array[i][0]==master->food[1]&&master->snake_array[i][1]==master->food[0])
+				    {
+				        tempflag1=1;
+					--(master->minitem);
+				        break;
+				    }
+				}
+				for(i=0;i<slave->snakelength;i++)
+				{
+				    if(slave->snake_array[i][0]==master->food[1]&&slave->snake_array[i][1]==master->food[0])
+				    {
+				        tempflag1=1;
+					--(slave->minitem);
+				        break;
+				    }
+				}
+
+				if(tempflag1==1)
+				{
+				    master->tempflag2=1;
+				    slave->tempflag2=1;
+				
+				}
+				else
+				{
+				    master->tempflag2=0;
+				    slave->tempflag2=0;
+				}
+			    }
+
+			}
+					attrset(COLOR_PAIR(1)|A_BOLD);
+			movesnake(master->x,master->y,master->snakelength,master->snake_array);
+			attroff(COLOR_PAIR(1)|A_BOLD);
+
+			attrset(COLOR_PAIR(2)|A_BOLD);
+			movesnake(slave->x,slave->y,slave->snakelength,slave->snake_array);
+			attroff(COLOR_PAIR(2)|A_BOLD);
+
+
+			foodprint(master->food);
+			master->flag=checkcollision(master->snake_array,master->snakelength,slave->snake_array,slave->snakelength);
+			//collision function change karna h........
+			if((foodeaten(master->food,master->snake_array[0][1],master->snake_array[0][0])==0))
+			{
+			    (master->snakelength)++;  //snakelength badh rhi h...already implemented feature
+			    (master->score)+=10;
+			    master->tempflag2=1;
+			    --(master->minitem);
+			}
+
+
+			//	reading direction keys from keyboard
+
+			if(kbhit())
+			{
+			    ch=readch();
+			    if((ch=='w')&&(master->x!=0)&&(master->y!=1))
+			    {
+				master->x=0;
+				master->y=-1;
+			    }
+			    if((ch=='s')&&(master->x!=0)&&(master->y!=-1))
+			    {
+				master->x=0;
+				master->y=1;
+			    }
+			    if((ch=='a')&&(master->x!=1)&&(master->y!=0))
+			    {	
+				master->x=-1;
+				master->y=0;
+			    }
+			    if((ch=='d')&&(master->x!=-1)&&(master->y!=0))
+			    {
+				master->x=1;
+				master->y=0;
+			    }
+
+			}
+
+
+			if(master->minitem<=0 && slave->minitem<=0)
+			{
+				if(foodcount==0)
+				{
+					++level;
+					flaggy=0;
+				}
+			}
+			else
+			{
+			//else print karana h ki level cross hi ni kiya
+				flaggy=0; //final flag for determining end of game or start of next level
+			}
 			
+			//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22
+			my_score=master->score;
+				other_score=slave->score;
+
+				//live score
+				printscore(master->score,slave->score);
+				usleep(100000);
+				erase();
 			break;
 			//set by ncurses display timelimit 6min;
 		case 2: foodcount=7;
-			minitem=2;
+			master->minitem=2;
+			slave->minitem=2;
 			//reading slave snake attribites
 			//reading slave snake attribites
 			read(slave_sockfd, slave, sizeof(snake));
@@ -273,9 +402,132 @@ int main()
 			hline(ACS_HLINE,COLS);
 
 			init_keyboard();
+			master->tempflag2=1;
+			slave->tempflag2=1;
+			while((master->tempflag2==1)||(slave->tempflag2==1)||foodcount!=0)                  //food generation
+			{
+			    srand(time(0));
+			    master->food[0]=(rand()%COLS-1);
+			    master->food[1]=(rand()%((5*LINES/6)-1));
+
+			    if(master->food[1]<=0||master->food[0]<=0)
+			    {
+				master->tempflag2=1;
+				slave->tempflag2=1;
+			    }
+			    else
+			    {
+
+				--foodcount;				
+				for(i=0;i<master->snakelength;i++)
+				{
+				    if(master->snake_array[i][0]==master->food[1]&&master->snake_array[i][1]==master->food[0])
+				    {
+				        tempflag1=1;
+					--(master->minitem);
+				        break;
+				    }
+				}
+				for(i=0;i<slave->snakelength;i++)
+				{
+				    if(slave->snake_array[i][0]==master->food[1]&&slave->snake_array[i][1]==master->food[0])
+				    {
+				        tempflag1=1;
+					--(slave->minitem);
+				        break;
+				    }
+				}
+
+				if(tempflag1==1)
+				{
+				    master->tempflag2=1;
+				    slave->tempflag2=1;
+				
+				}
+				else
+				{
+				    master->tempflag2=0;
+				    slave->tempflag2=0;
+				}
+			    }
+
+			}
+					attrset(COLOR_PAIR(1)|A_BOLD);
+			movesnake(master->x,master->y,master->snakelength,master->snake_array);
+			attroff(COLOR_PAIR(1)|A_BOLD);
+
+			attrset(COLOR_PAIR(2)|A_BOLD);
+			movesnake(slave->x,slave->y,slave->snakelength,slave->snake_array);
+			attroff(COLOR_PAIR(2)|A_BOLD);
+
+
+			foodprint(master->food);
+			master->flag=checkcollision(master->snake_array,master->snakelength,slave->snake_array,slave->snakelength);
+			//collision function change karna h........
+			if((foodeaten(master->food,master->snake_array[0][1],master->snake_array[0][0])==0))
+			{
+			    (master->snakelength)++;  //snakelength badh rhi h...already implemented feature
+			    (master->score)+=10;
+			    master->tempflag2=1;
+			    --(master->minitem);
+			}
+
+
+			//	reading direction keys from keyboard
+
+			if(kbhit())
+			{
+			    ch=readch();
+			    if((ch=='w')&&(master->x!=0)&&(master->y!=1))
+			    {
+				master->x=0;
+				master->y=-1;
+			    }
+			    if((ch=='s')&&(master->x!=0)&&(master->y!=-1))
+			    {
+				master->x=0;
+				master->y=1;
+			    }
+			    if((ch=='a')&&(master->x!=1)&&(master->y!=0))
+			    {	
+				master->x=-1;
+				master->y=0;
+			    }
+			    if((ch=='d')&&(master->x!=-1)&&(master->y!=0))
+			    {
+				master->x=1;
+				master->y=0;
+			    }
+
+			}
+
+
+			if(master->minitem<=0 && slave->minitem<=0)
+			{
+				if(foodcount==0)
+				{
+					++level;
+					flaggy=0;
+				}
+			}
+			else
+			{
+			//else print karana h ki level cross hi ni kiya
+				flaggy=0; //final flag for determining end of game or start of next level
+			}
+			
+			//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22
+			my_score=master->score;
+				other_score=slave->score;
+
+				//live score
+				printscore(master->score,slave->score);
+				usleep(100000);
+				erase();
 			break;
 		case 3: foodcount=8;
-			minitem=2;
+			master->minitem=2;
+			slave->minitem=2;
 			//reading slave snake attribites
 			read(slave_sockfd, slave, sizeof(snake));
 
@@ -290,6 +542,128 @@ int main()
 			hline(ACS_HLINE,COLS);
 
 			init_keyboard();
+			master->tempflag2=1;
+			slave->tempflag2=1;
+			while((master->tempflag2==1)||(slave->tempflag2==1)||foodcount!=0)                  //food generation
+			{
+			    srand(time(0));
+			    master->food[0]=(rand()%COLS-1);
+			    master->food[1]=(rand()%((5*LINES/6)-1));
+
+			    if(master->food[1]<=0||master->food[0]<=0)
+			    {
+				master->tempflag2=1;
+				slave->tempflag2=1;
+			    }
+			    else
+			    {
+
+				--foodcount;				
+				for(i=0;i<master->snakelength;i++)
+				{
+				    if(master->snake_array[i][0]==master->food[1]&&master->snake_array[i][1]==master->food[0])
+				    {
+				        tempflag1=1;
+					--(master->minitem);
+				        break;
+				    }
+				}
+				for(i=0;i<slave->snakelength;i++)
+				{
+				    if(slave->snake_array[i][0]==master->food[1]&&slave->snake_array[i][1]==master->food[0])
+				    {
+				        tempflag1=1;
+					--(slave->minitem);
+				        break;
+				    }
+				}
+
+				if(tempflag1==1)
+				{
+				    master->tempflag2=1;
+				    slave->tempflag2=1;
+				
+				}
+				else
+				{
+				    master->tempflag2=0;
+				    slave->tempflag2=0;
+				}
+			    }
+
+			}
+					attrset(COLOR_PAIR(1)|A_BOLD);
+			movesnake(master->x,master->y,master->snakelength,master->snake_array);
+			attroff(COLOR_PAIR(1)|A_BOLD);
+
+			attrset(COLOR_PAIR(2)|A_BOLD);
+			movesnake(slave->x,slave->y,slave->snakelength,slave->snake_array);
+			attroff(COLOR_PAIR(2)|A_BOLD);
+
+
+			foodprint(master->food);
+			master->flag=checkcollision(master->snake_array,master->snakelength,slave->snake_array,slave->snakelength);
+			//collision function change karna h........
+			if((foodeaten(master->food,master->snake_array[0][1],master->snake_array[0][0])==0))
+			{
+			    (master->snakelength)++;  //snakelength badh rhi h...already implemented feature
+			    (master->score)+=10;
+			    master->tempflag2=1;
+			    --(master->minitem);
+			}
+
+
+			//	reading direction keys from keyboard
+
+			if(kbhit())
+			{
+			    ch=readch();
+			    if((ch=='w')&&(master->x!=0)&&(master->y!=1))
+			    {
+				master->x=0;
+				master->y=-1;
+			    }
+			    if((ch=='s')&&(master->x!=0)&&(master->y!=-1))
+			    {
+				master->x=0;
+				master->y=1;
+			    }
+			    if((ch=='a')&&(master->x!=1)&&(master->y!=0))
+			    {	
+				master->x=-1;
+				master->y=0;
+			    }
+			    if((ch=='d')&&(master->x!=-1)&&(master->y!=0))
+			    {
+				master->x=1;
+				master->y=0;
+			    }
+
+			}
+
+
+			if(master->minitem<=0 && slave->minitem<=0)
+			{
+				if(foodcount==0)
+				{
+					++level;
+					flaggy=0;
+				}
+			}
+			else
+			{
+			//else print karana h ki level cross hi ni kiya
+				flaggy=0; //final flag for determining end of game or start of next level
+			}
+			
+			//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22
+			my_score=master->score;
+				other_score=slave->score;
+
+				//live score
+				printscore(master->score,slave->score);
+				usleep(100000);
+				erase();
 			break;
 	}
 }while(flaggy);//kaafi conditions iski wali hatayi h coz they were related to collision
